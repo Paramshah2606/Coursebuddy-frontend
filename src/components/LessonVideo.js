eaa5f9c09ea8a9a24a751b29f605132f10bdd651
unknown <paramshah2606@gmail.com>
2025-05-22 18:27:18 +0530

    Prod fixings

--- src/components/LessonVideo.js
+++ src/components/LessonVideo.js
@@ -3,7 +3,7 @@
 export default function LessonVideo({link}){
 let splitLink=link.split("https://youtu.be/")[1];
 let id=splitLink.split("?")[0];
-var embedlink = "http://www.youtube.com/embed/" + id
+var embedlink = "https://www.youtube.com/embed/" + id
     return (
         <div>
             <iframe width="560" height="315" src={embedlink} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
